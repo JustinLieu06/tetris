2df00918fbeb9a90af0ad01ab9a90d8aa97ff48a
JustinLieu06 <justinlieu06@gmail.com>
2020-06-02 19:39:19 -0700

    added edges to board

--- tetris.js
+++ tetris.js
@@ -9,6 +9,21 @@ const matrix = [
   [0, 1, 0],
 ];
 
+function collide(arena, player){
+  //m is matrix, o is offset
+  const [m, o] = [player.matrix, player.pos];
+  for (let y = 0; y < m.length; ++y){
+    for (let x = 0; x < m[y].length; ++x){
+      if (m[y][x] !== 0 && 
+          (arena[y + o.y] 
+            && arena[y + o.y][x + o.x]) !== 0){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 function createMatrix(w, h){
   const matrix = [];
   while (h--){
@@ -21,7 +36,8 @@ function draw(){
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
-  drawMatrix(player.matrix, player.pos)
+  drawMatrix(arena, {x: 0, y: 0});
+  drawMatrix(player.matrix, player.pos);
 }
 
 function drawMatrix(matrix, offset) {
@@ -33,18 +49,6 @@ function drawMatrix(matrix, offset) {
       }
     });
   });
-}
-
-function collide(arena, player){
-  const [m, offset] = [player.matrix, player.pos];
-  for (let y = 0; y < m.length; ++y){
-    for (let x = 0; x < m[y].length; ++x){
-      if (m[y][x] !== 0 && arena[y + offset.y] && arena[y + offset.y][x + o.x] !== 0){
-        return true;
-      }
-    }
-  }
-  return false;
 }
 
 function merge(arena, player){
@@ -60,11 +64,19 @@ function merge(arena, player){
 function playerDrop(){
   player.pos.y++;
   if (collide(arena, player)){
+    console.log('collide');
     player.pos.y--;
     merge(arena, player);
     player.pos.y = 0;
   }
   dropCounter = 0;
+}
+
+function playerMove(dir){
+  player.pos.x += dir;
+  if (collide(arena, player)){
+    player.pos.x -= dir;
+  }
 }
 
 let dropCounter = 0;
@@ -95,10 +107,10 @@ const player = {
 document.addEventListener('keydown', event => {
   // console.log(event);
   if (event.keyCode === 37){
-    player.pos.x--;
+    playerMove(-1);
   }
   else if (event.keyCode === 39){
-    player.pos.x++;
+    playerMove(1);
   }
   else if (event.keyCode === 40){
     playerDrop();
