172eb1144c9f518144dcf561d22b4057d3bbfdfe
JustinLieu06 <justinlieu06@gmail.com>
2020-06-04 16:52:03 -0700

    added player rotation

--- tetris.js
+++ tetris.js
@@ -79,6 +79,10 @@ function playerMove(dir){
   }
 }
 
+function playerRotate(dir){
+  rotate(player.matrix, dir);
+}
+
 function rotate(matrix, dir){
   for (let y = 0; y < matrix.length; ++y){
     for (let x = 0; x < y; ++x){
@@ -134,6 +138,12 @@ document.addEventListener('keydown', event => {
   }
   else if (event.keyCode === 40){
     playerDrop();
+  }
+  else if (event.keyCode === 81){
+    playerRotate(-1);
+  }
+  else if (event.keyCode === 69){
+    playerRotate(1);
   }
 });
 
